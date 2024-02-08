5a4f34a5420b0b6def0770c1a65c7758b27645e6
Alexis-Papazoglou <alex.papazoglu99@gmail.com>
2024-02-08 08:15:11 +0200

    added dummy firebase file

---
+++ Firebase-Change-This.js
@@ -0,0 +1,18 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth, initializeAuth, indexedDBLocalPersistence } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "",
+//     projectId: "",
+//     storageBucket: "n",
+//     messagingSenderId: "",
+//     appId: ""
+// };
+
+// // Initialize Firebase
+// export const FIREBASE_APP = initializeApp(firebaseConfig);
+// export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, { persistence: indexedDBLocalPersistence });
+// export const FIREBASE_DB = getFirestore(FIREBASE_APP);
